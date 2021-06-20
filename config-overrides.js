2fed770b2498f6ff6ff362cfb3f466c2d284a7fa
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-06-20 16:07:24 +0200

    🔧 Configure eslint and webpack aliases (#3)

---
+++ config-overrides.js
@@ -0,0 +1,4 @@
+const { override } = require('customize-cra');
+const { alias, configPaths } = require('react-app-rewire-alias');
+
+module.exports = override(alias(configPaths('./tsconfig.paths.json')));
---
+++ src/application/app/app.component.tsx
@@ -0,0 +1,3 @@
+export function App(): JSX.Element {
+  return <h1>App</h1>;
+}
