aea61e9a76febf14b3638ac8976311c0b954c0cd
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-06-20 19:51:38 +0200

    cleanup

--- src/application/app/app.spec.tsx
+++ src/application/app/app.spec.tsx
@@ -2,15 +2,9 @@ import { render } from '@testing-library/react';
 
 import { App } from './app.component';
 
-const wrapComponent = (props = {}) => {
-  const defaultProps = {};
-
-  return render(<App {...defaultProps} {...props} />);
-};
-
 describe('<App /> Component', () => {
   test('should render without crashing', () => {
-    const { container } = wrapComponent();
+    const { container } = render(<App />);
 
     expect(container).toBeInTheDocument();
   });
