89cff9428d894673c770766a6cd3b0bdad54ddd7
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-06-22 21:05:33 +0200

    🔧 Configure storybook (#9)

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,4 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override } = require('customize-cra');
 const { alias, configPaths } = require('react-app-rewire-alias');
 
-module.exports = override(
-  alias(configPaths('./tsconfig.paths.json')),
-);
+module.exports = override(alias(configPaths('./tsconfig.paths.json')));
--- src/ui/atoms/big-heading/big-heading.types.ts
+++
@@ -1,3 +0,0 @@
-export interface StyledBigHeadingProps {
-  pointer?: boolean;
-}
