91db3a5f6b2c51ec727433f6b24491a2404f19ca
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-06-23 23:55:41 +0200

    fix multiple objects calculation

--- src/domain/dashboard/utils/functions/set-button-position/set-button-position.function.ts
+++ src/domain/dashboard/utils/functions/set-button-position/set-button-position.function.ts
@@ -7,7 +7,7 @@ export const setButtonPosition =
       return;
     }
 
-    const { id, top, left, _objects: objects } = e.target;
+    const { id, top, left, scaleX, scaleY, _objects: objects } = e.target;
 
     if (!objects) {
       const foundButton = document.getElementById(id);
@@ -22,8 +22,8 @@ export const setButtonPosition =
         const foundButton = document.getElementById(targetId);
 
         if (foundButton) {
-          foundButton.style.top = `${top + _target.top - buttonHeight / 2}px`;
-          foundButton.style.left = `${left + _target.left - buttonWidth / 2}px`;
+          foundButton.style.top = `${top + _target.top * scaleY - buttonHeight / 2}px`;
+          foundButton.style.left = `${left + _target.left * scaleX - buttonWidth / 2}px`;
         }
       });
     }
