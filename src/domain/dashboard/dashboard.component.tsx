b28f62ec5167fe0b1ce1d1bf496daf9f97c052e4
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-07-03 14:39:45 +0200

    🔧 Configure lint-staged (#17)

--- src/domain/dashboard/dashboard.component.tsx
+++ src/domain/dashboard/dashboard.component.tsx
@@ -1,13 +1,12 @@
 import { fabric } from 'fabric';
 import { useState, useEffect, ReactNode } from 'react';
 
+import { getWindowWidth, getWindowHeight, useDebounce, useEventListener } from '@utils';
+import { CANVAS_WIDTH_SCALE, CANVAS_BUTTONS_CONTAINER_ID } from '@infrastructure';
 import { Canvas } from './atoms';
 import { Toolbox, LoadingPage } from './molecules';
 
 import { bindEvents, useFabricJsEditor } from './utils';
-import { getWindowWidth, getWindowHeight, useDebounce, useEventListener } from '@utils';
-
-import { CANVAS_WIDTH_SCALE, CANVAS_BUTTONS_CONTAINER_ID } from '@infrastructure';
 
 export const Dashboard = (): JSX.Element => {
   const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
--- src/utils/functions/is-number/is-number.function.ts
+++ src/utils/functions/is-number/is-number.function.ts
@@ -1,9 +1,9 @@
 export function isNumber(value: any): value is number {
-  if (typeof value == 'number') {
+  if (typeof value === 'number') {
     return value - value === 0;
   }
 
-  if (typeof value == 'string' && value.trim() !== '') {
+  if (typeof value === 'string' && value.trim() !== '') {
     return Number.isFinite(+value);
   }
 
